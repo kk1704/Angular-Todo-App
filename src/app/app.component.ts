46138e65c11318aa95a7a465c602b20e655f4ed2
Kundan <kundan.k1988@gmail.com>
2023-08-31 23:46:02 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular-Todo-App';
+
+  getTasks() {
+  alert("All Tasks")
+  }
+
+  createTasks() {
+  alert("Create Tasks")
+  }
+}
